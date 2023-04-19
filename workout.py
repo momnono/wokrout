144a12abfcc628bf575e2301ebc48c887c5eda2b
mo <alshmoha@shernet.sheridancollege.ca>
2023-04-19 04:13:44 -0400

    created methods and classes to be used

---
+++ workout.py
@@ -0,0 +1,28 @@
+"""a app that manages a collection of workouts"""
+import json
+
+class Workout:
+    def __init__(self, name, muscle_group, reps_sets, weight, difficulty):
+        pass
+
+class WorkoutManager:
+    def __init__(self, data_file):
+        pass
+
+    def create_workout(self):
+        pass
+
+    def search_workouts(self):
+        pass
+
+    def edit_workout(self):
+        pass
+
+    def delete_workout(self):
+        pass
+
+    def save_data(self):
+        pass
+
+    def run(self):
+        pass
