a22f4727f67445b29442126b999352d650682557
mo <alshmoha@shernet.sheridancollege.ca>
2023-04-19 18:59:19 -0400

    added metho to creat and serch for workouts

--- workout.py
+++ workout.py
@@ -2,26 +2,52 @@
 import json
 
 class Workout:
-    def __init__(self, name, muscle_group, reps_sets, weight, difficulty):
+    def __init__(self, name, muscle_group, reps_sets, weight, difficulty):        # Initializes a new workout object with the given properties.
+
         self.name = name
         self.muscle_group = muscle_group
         self.reps_sets = reps_sets
         self.weight = weight
         self.difficulty = difficulty
 
-    def __str__(self):
+    def __str__(self):        # Returns a string representation of the workout object.
+
         return f"{self.name} ({self.muscle_group})"
 
 
 class WorkoutManager:
     def __init__(self, data_file):
-        pass
+        self.data_file = data_file
+        self.workouts = []
+        try:
+            with open(self.data_file, "r") as f:
+                data = json.load(f)
+                for w in data["workouts"]:
+                    self.workouts.append(Workout(**w))
+        except FileNotFoundError:
+            pass
 
     def create_workout(self):
-        pass
+        name = input("Name: ")
+        muscle_group = input("Muscle group: ")
+        reps_sets = input("Reps/Sets: ")
+        weight = input("Weight: ")
+        difficulty = input("Difficulty: ")
+        workout = Workout(name, muscle_group, reps_sets, weight, difficulty)
+        self.workouts.append(workout)
+        print("Workout created successfully.")
+
+
 
     def search_workouts(self):
-        pass
+        search_term = input("Enter a search term: ")
+        matching_workouts = [w for w in self.workouts if search_term.lower() in w.name.lower() or search_term.lower() in w.muscle_group.lower()]
+        if matching_workouts:
+            for workout in matching_workouts:
+                print(f"{workout.name} ({workout.muscle_group})")
+        else:
+            print("No matching workouts found.")
+
 
     def edit_workout(self):
         pass
